7de01a4ee1ee9445443120290617c63900530654
Andre Amor <ramorSTEM2022@gmail.com>
2025-07-03 18:23:13 -0700

    start

---
+++ src/astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+});
